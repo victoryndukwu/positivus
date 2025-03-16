572eccbea287936123e693448cac4d0ec78b2814
Victory Ndukwu <victoryndukwu@Victorys-MacBook-Pro-M3.local>
2025-03-16 16:00:09 +0100

    chore: add motion to the project

--- components/button/button.tsx
+++ components/button/button.tsx
@@ -1,12 +1,14 @@
+"use client";
 import { cn } from "@/utils";
 import React from "react";
+import { motion } from "motion/react";
 
 const buttonVariants = {
-  base: "py-5 px-[35px] rounded-[14px] font-normal cursor-pointer text-[20px]",
+  base: "py-5 px-[35px] rounded-[14px] font-normal cursor-pointer text-[20px] cursor-pointer transition-all duration-100",
   variants: {
-    primary: "bg-[#b9ff66] text-black",
+    primary: "bg-[#b9ff66] text-black ",
     black: "bg-[#191a23] text-white",
-    outline: "border border-[#191a23] bg-white text-[#191a23]",
+    outline: "border border-[#191a23] bg-white text-[#191a23] hover:bg-[#b9ff66]",
   },
 };
 
@@ -21,7 +23,7 @@ interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
   ({ children, className, variant = "black", ...props }, ref) => {
     return (
-      <button
+      <motion.button
         type="button"
         className={cn(
           buttonVariants.base,
@@ -30,9 +32,11 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
         )}
         ref={ref}
         {...props}
+        whileHover={{ scale: 1.05 }}
+        whileTap={{ scale: 0.95 }}
       >
         {children}
-      </button>
+      </motion.button>
     );
   }
 );
--- components/sections/hero.tsx
+++ components/sections/hero.tsx
@@ -1,14 +1,18 @@
+"use client";
 import React from "react";
-import Button from "../button/button";
 import Image from "next/image";
+import Button from "../button/button";
+import { motion } from "motion/react";
 
 function Hero() {
+
+ 
   return (
     <section className="flex justify-between items-start gap-2 w-full">
       <div className="w-1/2 h-full pr-20 flex flex-col gap-[35px]">
-        <h2 className="text-[60px]">
+        <motion.h2 className="text-[60px]" animate={{scale: 1}}>
           Navigating the digital landscape for success
-        </h2>
+        </motion.h2>
         <p className="text-[20px]">
           Our digital marketing agency helps businesses grow and succeed online
           through a range of services including SEO, PPC, social media
