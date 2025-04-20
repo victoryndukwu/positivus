739c4cb07961bd283aa298e54af6e74c1ed248f1
Victory Ndukwu <victoryndukwu@Victorys-MacBook-Pro-M3.local>
2025-04-20 19:13:47 +0100

    chore: fix the motion error on the button

--- components/button/button.tsx
+++ components/button/button.tsx
@@ -1,10 +1,11 @@
 "use client";
 import { cn } from "@/utils";
 import React from "react";
-import { motion } from "motion/react";
+import { motion, HTMLMotionProps } from "framer-motion"; // <-- make sure it's "framer-motion", not "motion/react"
 
 const buttonVariants = {
-  base: "py-5 px-[35px] rounded-[14px] font-normal cursor-pointer text-base md:text-[20px] cursor-pointer transition-all duration-100",
+  base:
+    "py-5 px-[35px] rounded-[14px] font-normal cursor-pointer text-base md:text-[20px] transition-all duration-100",
   variants: {
     primary: "bg-[#b9ff66] text-black ",
     black: "bg-[#191a23] text-white",
@@ -14,7 +15,7 @@ const buttonVariants = {
 
 type Variant = keyof typeof buttonVariants.variants;
 
-interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+interface ButtonProps extends HTMLMotionProps<"button"> {
   children: React.ReactNode;
   variant?: Variant;
   className?: string;
@@ -24,16 +25,15 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
   ({ children, className, variant = "black", ...props }, ref) => {
     return (
       <motion.button
-        type="button"
         className={cn(
           buttonVariants.base,
           buttonVariants.variants[variant],
           className
         )}
         ref={ref}
-        {...props}
         whileHover={{ scale: 1.05 }}
         whileTap={{ scale: 0.95 }}
+        {...props}
       >
         {children}
       </motion.button>
